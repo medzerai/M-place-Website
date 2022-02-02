1b5587f7280eac60c0e28412a8a6c0e231b90421
NeirouzJbira <jbira.neirouz@gmail.com>
2022-02-02 14:03:26 +0100

    init

--- server.js
+++ server.js
@@ -1,46 +1,31 @@
 import express from "express";
+const cors = require("cors");
+
 const app = express();
-import "express-async-errors";
-// const cors = require("cors");
-import cors from "cors";
 app.use(cors());
+require("dotenv").config();
 
-import dotenv from "dotenv";
-dotenv.config();
+require("./server/config/mongoose.config");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require("./server/routes/authRoutes")(app);
 
-// connect DB
-import connectDB from "./server/config/connectDB.js";
-
-// Routes
-import authRouter from "./server/routes/authRoutes.js";
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Listening at Port 8000");
+});
 
 // middleware
 import notFoundMiddleware from "./server/middleware/not-found.js";
 import errorHandlerMiddleware from "./server/middleware/error-handler.js";
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send("Welcome!");
-});
-
-app.use("/api/v1/auth", authRouter);
-
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
-
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URL);
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+//Error handler
+app.use((req, res, next) => {
+  res.status(404);
+  res.send({
+    error: "not found",
+  });
+});
