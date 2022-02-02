ac3a5575149ec8973fab83593bef6505e5c86854
medzerai <mohamed0zerai@gmail.com>
2022-02-02 12:34:14 +0100

    missing fields error handling

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -6,7 +6,15 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     message: "Something went wrong, try again later",
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.message = err.message;
+    defaultError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.message });
 };
 
 export default errorHandlerMiddleware;
