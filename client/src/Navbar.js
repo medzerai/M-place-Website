a73d1ee34b0b292e18148ff341757425d91bfbb3
fereszareb <fereszareb@gmail.com>
2022-02-03 03:09:07 +0100

    navbar categories from json

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -2,6 +2,47 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/navbar.css";
 
+var json = '[{"id":1,"categorie":"categ1","sous-categorie":[{"id":120,"titre":"sous-categ1","sous-sous-categ":[{"name":"nn1"},{"name":"nn2"},{"name":"nn3"},{"name":"nn4"}]},{"id":120,"titre":"sous-categ1","sous-sous-categ":[{"name":"nn1"},{"name":"nn2"},{"name":"nn3"},{"name":"nn4"}]},{"id":120,"titre":"sous-categ1","sous-sous-categ":[{"name":"nn1"},{"name":"nn2"},{"name":"nn3"},{"name":"nn4"}]},{"id":121,"titre":"sous-categ2","sous-sous-categ":[{"name":"kn5"},{"name":"nkndhdgd45"}]}]},{"id":2,"categorie":"categ2","sous-categorie":[{"id":122,"titre":"sous-categ3","sous-sous-categ":[{"name":"sjsh"},{"name":"uhded"},{"name":"szohudige"}]},{"id":123,"titre":"sous-categ4","sous-sous-categ":[{"name":"sqszsz"},{"name":"efrf"},{"name":"rfrtgrt"},{"name":"eferfe"}]}]}]'
+json = JSON.parse(json);
+
+
+
+ 
+function hoverCateg(e){
+  const categs = document.querySelectorAll('.item-categ');
+  for(const c of categs){
+    c.className="item-categ";
+  }
+  e.target.className="item-categ active";
+  var sous_categ='';
+  const id= e.target.getAttribute('data-id');
+  for(var i=0; i<json.length;i++){ 
+    if (json[i]["id"] == id){
+      for(var j=0;j<json[i]["sous-categorie"].length;j++){
+        sous_categ += '<div class="sous-categorie-bloc"> <a href="/'+json[i]["categorie"]+'/'+json[i]["sous-categorie"][j]["titre"]+'" class="title-sous-categ">'+json[i]["sous-categorie"][j]["titre"]+'</a>';
+        for(var k=0;k<json[i]["sous-categorie"][j]["sous-sous-categ"].length;k++){
+          sous_categ += '<a href="/'+json[i]["categorie"]+'/'+json[i]["sous-categorie"][j]["titre"]+'/'+json[i]["sous-categorie"][j]["sous-sous-categ"][k]["name"]+'">'+json[i]["sous-categorie"][j]["sous-sous-categ"][k]["name"]+"</a>";
+        } 
+        sous_categ+='</div>';
+      } 
+      document.querySelector(".sous-categ").innerHTML = sous_categ;
+      break;
+    }
+  }
+}  
+
+function loadNavbar(kk){
+  if(kk ==0){
+    return(
+      <div>hello 0</div>
+    )
+  }else{
+    return(
+      <div>hello other</div>
+    )
+  }
+}
+
 const Navbar = () => {
   return (
     <div className="header">
@@ -77,11 +118,25 @@ const Navbar = () => {
           <div className="row position-relative">
             <div className="col-2">
               <div className="categorie">Categories</div>
-              <ul className="dropdown-categorie position-absolute">
-                <li>categ 1</li>
-                <li>categ 2</li>
-                <li>categ 3</li>
-              </ul>
+              <div className="dropdown-categorie position-absolute">
+                <div className="row margin-left-0">
+                  <div className="col-200">
+                    <ul className="liste-categorie">
+                      { 
+                        json.map((item,key)=>{
+                          return(
+                            <li className="item-categ" key={item.id} data-id={item.id} onMouseOver={hoverCateg}>{item.categorie}</li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </div>
+                  <div className="col-700">
+                    <div className="sous-categ"> 
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="col-6"></div>
             <div className="col-4"></div>
