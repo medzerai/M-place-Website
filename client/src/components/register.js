a8820ffdd74a71cacf9b33dd1c70e6f626051fd5
NeirouzJbira <jbira.neirouz@gmail.com>
2022-02-07 11:05:25 +0100

    init

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,8 +1,6 @@
-import {React} from 'react';
+import { React } from "react";
 const Register = () => {
-    return ( 
-        <div>Register</div>
-     );
-}
- 
+  return <div>Register</div>;
+};
+
 export default Register;
