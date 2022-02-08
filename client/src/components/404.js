4730ef8e44fbbbaf41a306ce998811b204ccd728
fereszareb <fereszareb@gmail.com>
2022-02-08 23:21:09 +0100

    design page 404

--- client/src/components/404.js
+++ client/src/components/404.js
@@ -1,7 +1,16 @@
 import {React} from 'react';
+import './../css/404.css';
+import NotFound from './../404.svg';
+import { useHistory } from "react-router-dom";
 const Erreur404 = () => {
+    let history = useHistory();
     return ( 
-        <div>Erreur 404</div>
+        <div className='NotFound'>
+            <img src={NotFound} draggable="false"/>
+            <button onClick={history.goBack} className="btn btn-primary d-block w-100">
+            Go back
+          </button>
+        </div>
      );
 }
  
