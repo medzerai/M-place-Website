352ef66ca7b695a2b865d2510270120b538a2f7a
medzerai <mohamed0zerai@gmail.com>
2022-02-09 15:36:23 +0100

    auth.controller.js update user

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,6 @@ const UserRouter = express.Router();
 
 UserRouter.route("/register").post(register);
 UserRouter.route("/login").post(login);
-UserRouter.route("/updateUser").patch(updateUser);
+UserRouter.route("/updateUser/:userId").patch(updateUser);
 
 export default UserRouter;
