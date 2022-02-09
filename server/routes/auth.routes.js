7b228fe6fe2213b9e21898b6df9391d372a3da3d
medzerai <mohamed0zerai@gmail.com>
2022-02-09 16:18:59 +0100

    edit authentication for updating

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { register, login, updateUser } from "../controllers/auth.controller.js";
-
 import express from "express";
+import auth from "../middleware/authenticateUser.js";
 const UserRouter = express.Router();
 
 UserRouter.route("/register").post(register);
 UserRouter.route("/login").post(login);
-UserRouter.route("/updateUser/:userId").patch(updateUser);
+UserRouter.route("/updateUser").patch(auth, updateUser);
 
 export default UserRouter;
