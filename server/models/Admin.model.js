e2644f024916a05fdce437e22d555bee6340fd23
medzerai <mohamed0zerai@gmail.com>
2022-03-07 12:18:05 +0100

    admin model

---
+++ server/models/Admin.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: { type: Boolean, default: false },
+});
+
+export default mongoose.model("Admin", AdminSchema);
