b2391f467e3717c7cda8064a15b8ccd9930298a1
medzerai <mohamed0zerai@gmail.com>
2022-03-08 16:27:38 +0100

    update admin model and create admin controller

--- server/models/Admin.model.js
+++ server/models/Admin.model.js
@@ -16,4 +16,16 @@ const AdminSchema = new mongoose.Schema({
   verified: { type: Boolean, default: false },
 });
 
+AdminSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+AdminSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+
 export default mongoose.model("Admin", AdminSchema);
