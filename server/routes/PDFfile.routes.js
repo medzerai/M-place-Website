190be1d60a68f21872cd6c93058cfe309ac5cbd3
medzerai <mohamed0zerai@gmail.com>
2022-03-16 17:41:16 +0100

    file upload controller

--- server/routes/PDFfile.routes.js
+++ server/routes/PDFfile.routes.js
@@ -1,4 +1,4 @@
-import { imageUpload } from "../config/multer.config.js";
+import { fileUpload } from "../config/multer.config.js";
 import {
   createImage,
   addImage,
@@ -9,8 +9,8 @@ import {
 import express from "express";
 const imageRouter = express.Router();
 
-imageRouter.post("/img/upload", imageUpload.any(), createImage);
-imageRouter.get("/imgs", getAllImages);
-imageRouter.get("/img/:id", getImageById);
+imageRouter.post("/file/upload", fileUpload.any(), createImage);
+imageRouter.get("/files", getAllImages);
+imageRouter.get("/file/:id", getImageById);
 
 export default imageRouter;
